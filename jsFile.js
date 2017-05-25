aecd5f669e8fdfc9081e39379d0e4562a834dd2c
dwong28 <dwong28@calpoly.edu>
2017-05-25 13:51:24 -0700

    Tabs Work
    Tabs work now, need to add stuff to them.

--- jsFile.js
+++ jsFile.js
@@ -1,4 +1,17 @@
 /**
  * Created by Dennis Wong on 5/18/2017.
  */
-var app = angular.module('mainPage', []);
+(function () {
+    var app = angular.module('mainPage', []);
+
+    app.controller('TabController', function() {
+      this.tab = 0;
+      this.isSet = function(newTab) {
+          return this.tab === newTab;
+      }
+
+      this.setTab = function(newTab) {
+          this.tab = newTab;
+      }
+    })
+})();
